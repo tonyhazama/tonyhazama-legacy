4bdae703782188059db7a05881eb30a6d6b216af
tonyhazama <tonyhazama4721@gmail.com>
2019-08-07 02:09:49 +0700

    added docs folder

---
+++ docs/precache-manifest.c42f1afecff9ff6189ffd48de335e049.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbe1e4082b6cf46f71d226859e6d46b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f2672f1761996f5bed",
+    "url": "/static/css/2.2a9dfeb4.chunk.css"
+  },
+  {
+    "revision": "8b258060bec06f0ba14b",
+    "url": "/static/css/main.dee1a343.chunk.css"
+  },
+  {
+    "revision": "66f2672f1761996f5bed",
+    "url": "/static/js/2.58a789b9.chunk.js"
+  },
+  {
+    "revision": "8b258060bec06f0ba14b",
+    "url": "/static/js/main.8548259b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "184e6ae962d4e781146dae588f44062b",
+    "url": "/static/media/bg.184e6ae9.jpg"
+  },
+  {
+    "revision": "8e8258b4f0e13ccbdec5d82fa7dc2aef",
+    "url": "/static/media/emblem.8e8258b4.png"
+  },
+  {
+    "revision": "c91e007d40bf9e980259d4b1c9a898ff",
+    "url": "/static/media/logo.c91e007d.png"
+  }
+]);
